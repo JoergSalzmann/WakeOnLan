69b5e2da467b1d41c79b9346573d322813d0e244
Jörg@ThinkPad <joerg_salzmann@gmx.de>
2024-12-05 19:25:06 +0100

    ConsoleServer-Testprojekt erstellt um BinarrayFormatter-Fehler zu simulieren

---
+++ ConsoleServer/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+var server = new WakeOnLan.Server();
+server.DebugMessage += (s, ea) => Console.WriteLine(ea.Message);
+server.WakeUpSucces += (s, ea) => Console.WriteLine(ea.Message);
+server.WakeUpSuccessMAC += (s, ea) => Console.WriteLine(ea.Message);
+server.ExceptionRaised += (s, ea) => Console.WriteLine(ea.Message);
+server.ListenChanged += (s, ea) => Console.WriteLine($"WOL aktiv: {ea.Value}");
+server.RequestFromClient += (e, ea) => Console.WriteLine(ea.Message);
+server.Start();
+
+Console.ReadKey();
+Console.WriteLine();
+server.Stop();
